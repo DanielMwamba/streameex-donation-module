382a910e52b4bfa70a7ecd05fb83dcc260085a96
Daniel Mwamba <danmuamba81@gmail.com>
2025-02-14 11:11:11 +0100

    feat: add  UI components for label, card, and tabs; update Stripe, Mobile-money and Payapl form with new UI elements

--- components/stripe-form.tsx
+++ components/stripe-form.tsx
@@ -1,54 +1,67 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use client"
+"use client";
 
-import { createStripeIdSession } from "@/services/stripe"
+import { type FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
 import { loadStripe } from "@stripe/stripe-js";
-import { useSession } from "next-auth/react"
-import { FormEvent, useState } from "react"
+import { createStripeIdSession } from "@/services/stripe";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ""
 );
 
 export default function StripeForm({ eventId }: { eventId: string }) {
-  const [loading, setLoading] = useState<boolean>(false)
-  const [amount, setAmount] = useState<number>(10)
-  const { data: session } = useSession()
+  const [loading, setLoading] = useState<boolean>(false);
+  const [amount, setAmount] = useState<number>(10);
+  const { data: session } = useSession();
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setLoading(true)
+    e.preventDefault();
+    setLoading(true);
     const sessionId = await createStripeIdSession({
       userId: (session?.user as any).id,
       eventId,
       redirectUrl: window.location.href,
       unitAmount: amount,
-      currency: 'usd'
-    })
+      currency: "usd",
+    });
     if (sessionId) {
-      const stripe = await stripePromise
+      const stripe = await stripePromise;
       if (stripe) {
-        await stripe.redirectToCheckout({ sessionId })
+        await stripe.redirectToCheckout({ sessionId });
       }
     }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   return (
-    <div className="w-full flex flex-col gap-3">
-      <form onSubmit={onSubmit} className="w-full flex flex-col gap-3">
-        <div className="w-full flex flex-col gap-2">
-          <label htmlFor="amount">Montant</label>
-          <div className="w-full relative">
-            <input required min={1} value={amount ?? 10} onChange={(e) => setAmount(Number(e.target.value))} type="number" name="amount" id="amount" className="block w-full px-3 py-2 border rounded-md shadow-sm appearance-none placeholder-ui-gray-medium border-ui-gray-medium focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm ph-no-capture text-gray-600" />
-          </div>
+    <div>
+      <form onSubmit={onSubmit} className="space-y-6">
+        <div className="space-y-2">
+          <Label htmlFor="amount" className="text-white">
+            Montant (USD)
+          </Label>
+          <Input
+            required
+            min={1}
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+            type="number"
+            id="amount"
+            className="bg-[#fff] text-black border-[#FF0099]/20 focus:border-[#FF0099]"
+          />
         </div>
-        <button disabled={loading} className="w-full bg-opacity-100 disabled:bg-opacity-50 bg-main p-2 text-white rounded-md">
-          {
-            loading ? "En cours..." : "Payer maintenant"
-          }
-        </button>
+        <Button
+          type="submit"
+          disabled={loading}
+          className="w-full font-semibold bg-[#FF0099] hover:bg-[#FF0099]/80"
+        >
+          {loading ? "En cours..." : "Payer avec Stripe"}
+        </Button>
       </form>
     </div>
-  )
+  );
 }
